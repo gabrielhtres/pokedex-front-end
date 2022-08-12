b8c59791f7bcda39362be7994daab19e03095fd6
Gabriel <gabrielhtres@gmail.com>
2022-08-12 11:21:59 -0300

    Iniciada configuração da navegação

--- App.js
+++ App.js
@@ -2,18 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button } from 'react-native';
 import { Styles } from './styles';
 import PokemonProfile from './PokemonProfile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  function renderizar() {
-    return <PokemonProfile />;
-  }
-
   return (
-    <View style={Styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title="Botão" onClick={renderizar}/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="PokemonProfile"
+          component={PokemonProfile}
+          options={{ title: 'Welcome' }}
+        />
+
+      
+        {/* https://reactnavigation.org/docs/use-navigation - Documentação navigation */}
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={Styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    //   <Button title="Botão" onClick={renderizar}/>
+    // </View>
   );
 }
--- PokemonProfile.js
+++ PokemonProfile.js
@@ -1,21 +1,29 @@
 import { Styles } from "./styles";
+import React, { useState, setState } from 'react';
+import { View, Text, Image, ScrollView, TextInput, Button } from 'react-native';
 
-class PokemonProfile {
-    constructor(props) {
-        this.state = {
-            pokemon: 'Teste'
-        };
+const PokemonProfile = (props) => {
+    const [pokemon, setPokemon] = useState(["Bulbassauro", "Charmander"]);
+    console.log(pokemon)
+
+    function mudarPokemons() {
+        console.log(pokemon)
+        if(pokemon[0] == "Bulbassauro") {
+            setPokemon(["Squirtle"]);
+        } else {
+            setPokemon(["Bulbassauro", "Charmander"]);
+        }
     }
 
-    componentDidMount() {
+    return(
+        <View>
+            <Text>{pokemon.map(item => {
+                return item + "\n";
+            })}</Text>
 
-    }
-
-    render() {
-        return(
-            <h1>{this.state.pokemon}</h1>
-        );
-    }
+            <Button title="Mudar" onPress={mudarPokemons}/>
+        </View>
+    );
 }
 
 export default PokemonProfile;
