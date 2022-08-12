151ff3fcd79e60202ed81dbe7b2129ff072d69dd
Gabriel <gabrielhtres@gmail.com>
2022-08-12 10:25:32 -0300

    Iniciados testes de navegação

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { Styles } from './styles';
+import PokemonProfile from './PokemonProfile';
 
 export default function App() {
+
+  function renderizar() {
+    return <PokemonProfile />;
+  }
+
   return (
     <View style={Styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Botão" onClick={renderizar}/>
     </View>
   );
 }
--- PokemonProfile.js
+++ PokemonProfile.js
@@ -1,1 +1,21 @@
 import { Styles } from "./styles";
+
+class PokemonProfile {
+    constructor(props) {
+        this.state = {
+            pokemon: 'Teste'
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <h1>{this.state.pokemon}</h1>
+        );
+    }
+}
+
+export default PokemonProfile;
