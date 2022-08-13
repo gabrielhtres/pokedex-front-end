db7d866f2eb380aa0b9cc1e7ee449930ff6b7e28
Gabriel <gabrielhtres@gmail.com>
2022-08-13 11:28:19 -0300

    Iniciada criação da estrutura da página

--- PokemonProfile.js
+++ PokemonProfile.js
@@ -14,14 +14,37 @@ const PokemonProfile = (props) => {
     }
 
     return(
-        <View>
-            <Text>{pokemon.map(item => {
-                return item + "\n";
-            })}</Text>
-            <Link to={{ screen: 'Home' }}>
-                <Button title="Mudar"/>
-            </Link>
-            
+        <View style={{
+            height: '100%',
+            backgroundColor: '#cef79f'
+        }}>
+            <View style={{
+                height: "40%",
+                backgroundColor: '#cef79f',
+            }}>
+                <Image
+                    source={{ uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" }}
+                    style={{ width: "100px", height: "100px"}}
+                />
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    marginTop: "auto",
+                    marginBottom: "5%"
+                }}>
+                    <Text style={{ color: "white" }}>Sobre</Text>
+                    <Text style={{ color: "white" }}>Status</Text>
+                    <Text style={{ color: "white" }}>Evolução</Text>
+                </View>
+            </View>
+            <View style={{
+                height: "60%",
+                backgroundColor: 'white',
+                borderTopLeftRadius: "20px",
+                borderTopRightRadius: "20px"
+            }}>
+
+            </View>
         </View>
     );
 }
